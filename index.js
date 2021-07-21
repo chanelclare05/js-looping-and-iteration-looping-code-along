9c01cb737892cf49b620688bcbc255399f65562b
Chanel Donahoe <cdonahoe05@gmail.com>
2021-07-21 11:11:46 +1000

    completed

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+
+function writeCards(arrayOfNames, eventName) {
+    let newArray = []
+    for (let i = 0 ; i<arrayOfNames.length ; i++){
+    newArray.push(`Thank you, ${arrayOfNames[i]}, for the wonderful ${eventName} gift!`)
+    }
+    return newArray;
+}
+
+function countDown(num) {
+    while ( num <= 10 && num >=0){
+        console.log(num--);
+    }
+}
